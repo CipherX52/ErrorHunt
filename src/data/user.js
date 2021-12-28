1b9b8b2cba7efe7a8d5754ec9680bfd3324c00a0
CipherX52 <32170371+CipherX52@users.noreply.github.com>
2021-12-28 07:23:43 +0530

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,19 @@
+var users=[{
+    "id":1,
+    "uid":"aiswaryawalter@gmail.com",
+    "pwd":"aiswarya"
+},
+{
+    "id":2,
+    "uid":"surya@gmail.com",
+    "pwd":"surya"
+},
+{
+    "id":3,
+    "uid":"bazith@gmail.com",
+    "pwd":"bazith"
+}
+]
+
+//changed the below line ('module.exports=user;' to 'module.exports=users;')
+module.exports=users;
