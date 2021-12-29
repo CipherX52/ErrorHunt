154d43361490f3827e7a0cb040e6df01b06ef3fe
CipherX52 <32170371+CipherX52@users.noreply.github.com>
2021-12-29 22:53:42 +0530

    reverted changes to book and author models

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library',
-        { useNewUrlParser: true },
-        { useMongoClient: true }
-        );
+mongoose.connect('mongodb://localhost:27017/Library');
 const Schema = mongoose.Schema;
 
 
